79e9f16da56153fe9c86706d1530e449c59a2c2a
Shivam Midha <129408883+shivam-707@users.noreply.github.com>
2026-07-01 23:42:32 +0530

    Add LotteryGame component to App

--- react-state/src/App.jsx
+++ react-state/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import TodoList from './TodoList'
+import LotteryGame from './LotteryGame'
 
 function App() {
   return (
     <>
       <TodoList />
+      <br></br><br></br>
+      <LotteryGame />
     </>
   )
 }
